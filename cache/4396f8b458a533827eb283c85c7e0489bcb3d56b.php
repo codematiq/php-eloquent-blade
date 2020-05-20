1500678ec85bfad1ee029861829d9c4db6db0ac5
Mindaugas B <info@gbit.lt>
2020-05-20 21:25:12 +0300

    Delete 4396f8b458a533827eb283c85c7e0489bcb3d56b.php

--- cache/4396f8b458a533827eb283c85c7e0489bcb3d56b.php
+++
@@ -1,5 +0,0 @@
-<?php $__currentLoopData = $pages; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $page): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <?php echo e($page->title); ?>
-
-<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php /**PATH /Users/code/Documents/Code/learn/eloquent/views/pages.blade.php ENDPATH**/ ?>
